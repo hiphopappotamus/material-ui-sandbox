fea696ea708d40825330fd8b74d62ccb1e41c5e8
Jamie Adam <jamie@kurtnoble.com>
2022-12-12 10:00:57 -0700

    add Head element

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Head from '/blocks/Head';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <><Head /><div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hi there yay!!
-        </a>
+        <Button variant="contained">hello world!</Button>
       </header>
-    </div>
+    </div></>
   );
 }
 
---
+++ src/blocks/Head.js
@@ -0,0 +1,9 @@
+function Head() {
+  return (
+    <head>
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+    </head>
+  )
+}
+
+export default Head;
